55623580a0139babbaa3d683a734c4dccac58053
Joseph Sung <chengyu.sung@gmail.com>
2025-08-25 23:22:14 +0800

    display name.

--- java/com/alibou/app/todo/impl/TodoServiceImplTest.java
+++ java/com/alibou/app/todo/impl/TodoServiceImplTest.java
@@ -1,11 +1,17 @@
 package com.alibou.app.todo.impl;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
+@DisplayName("TodoServiceImpl Unit Test")
 class TodoServiceImplTest {
+  @Test
+  void createTodos(){
 
+  }
 }
