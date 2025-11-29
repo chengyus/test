72ef827b8ae3736e853acc5d3ef200374b5d04b1
Joseph Sung <chengyu.sung@gmail.com>
2025-11-29 08:24:16 +0800

    data.sql idempotent

--- resources/data.sql
+++ resources/data.sql
@@ -1,4 +1,2 @@
-INSERT INTO public.categories (id,created_by,created_date,last_modified_by,last_modified_date,description,name) VALUES
-	 ('1','APP','2025-11-26 00:00:00','','2025-11-26 21:23:00','Work','Work');
-INSERT INTO public.roles (id,created_by,created_date,last_modified_by,last_modified_date,name) VALUES
-	 ('1','APP','2025-11-26 00:00:00',NULL,'2025-11-26 21:22:00','ROLE_USER');
+INSERT INTO public.categories (id,created_by,created_date,last_modified_by,last_modified_date,description,name) VALUES ('1','APP','2025-11-26 00:00:00','','2025-11-26 21:23:00','Work','Work') ON CONFLICT (id) DO NOTHING;
+INSERT INTO public.roles (id,created_by,created_date,last_modified_by,last_modified_date,name) VALUES ('1','APP','2025-11-26 00:00:00',NULL,'2025-11-26 21:22:00','ROLE_USER') ON CONFLICT (id) DO NOTHING;
